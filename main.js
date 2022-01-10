1877f1d35a3a296b1a88b4285b4deb04d658708e
Павел <akumapl86@gmail.com>
2022-01-10 15:34:15 +0300

    sync

--- main.js
+++ main.js
@@ -2,17 +2,38 @@ const {
     app,
     BrowserWindow
 } = require('electron');
-const path = require('path');
-const mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800,
-    webPreferences: {
-        nodeIntegration: true,
-        enableRemoteModule: true
-    },
-    icon: path.join(__dirname, 'src', 'assets', 'ok01.png'),
-    title: 'OGO App',
-});
-mainWindow.setTitle('OGO App');
+const path = require('path')
+const menu = require('./public/contextMenu');
 
-// mainWindow.loadFile(path.join(__dirname, '../path/to/html'));
+function createWindow() {
+    const win = new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            contextIsolation: false
+        },
+        icon: path.join(__dirname, 'src', 'assets', 'ok01.png'),
+        title: 'OGO',
+    });
+
+    win.setTitle('OGOapp');
+    win.loadFile('./public/mainWin.html');
+    win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
